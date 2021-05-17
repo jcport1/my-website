0beab0bfa24fbd44abb96d65ae8f7f5c0106d20c
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-05-16 23:53:43 -0400

    Removes logo png files

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Nav.Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+    return(
+
+        <Navbar bg="light" variant="light">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            
+            </Nav>
+        
+        </Navbar>
+    )
+}
