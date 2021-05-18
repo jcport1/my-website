d1894cd03df8bca7986e48685b0837065fab96bf
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-05-18 13:20:44 -0400

    Installs font awesome and updates hamburger menu icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 // import { Link } from 'react-scroll';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar(){
 
@@ -9,7 +11,7 @@ export default function Navbar(){
         <div className="container-fluid">
         <a className="navbar-brand" href="#">Navbar</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+        <FontAwesomeIcon icon={faBars} style={{ color: "#fff"}}/>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
