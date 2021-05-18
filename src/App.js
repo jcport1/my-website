c846cc71c9a312e9db0ce9aa2c603a3d21ec506c
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-05-18 09:31:01 -0400

    Installs bootsrap and imports file into app js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
 import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 import About from "./components/About"
