62b79fee7dcd56b76737f244117fe44b97616542
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-05-18 12:43:47 -0400

    Adds border top to nav items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,8 +14,8 @@ export default function Navbar(){
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <li className="nav-item active">
+            <a className="nav-link" aria-current="page" href="#">Home</a>
             </li>
             <li className="nav-item">
             <a className="nav-link" href="#">Projects</a>
