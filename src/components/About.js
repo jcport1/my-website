4da59ff90a0c54f26315e258369ad5e3eaad3781
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-05-19 10:04:53 -0400

    About Styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import author from "../jeff-image.jpeg"
+import author from "../jeff-image.jpg"
 
 const About = () => {
 
     return (
 
-        <div className="container py-5" id="about">
+        <div className="container py-5">
             <div className="row">
                 <div className="col-lg-6 col-xm-12">
-                    <img src={author} alt="author..."/>
+                    <div className="photo-wrap mb-5">
+                        <img className="profile-img" src={author} alt="author..."/>
+                     </div>
                 </div>
                 
                 <div className="col-lg-6 col-xm-12">
-                    <h1>About me</h1>
+                    <h1 className="about-heading">About me</h1>
                     <p>Full stack software engineer with experience in Ruby on Rails, JavaScript, and React. </p>
                 </div>
             </div>
