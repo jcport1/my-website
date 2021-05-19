6d4584d0e43ab2e0528e06fbce7cff643a8ab905
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-05-19 09:44:12 -0400

    Builds out About me component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css"
-import Header from "./components/Header"
-import Navbar from "./components/Navbar"
-import About from "./components/About"
-import Projects from "./components/Projects"
-import Resume from "./components/Resume"
-import Contact from "./components/Contact"
+import React from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
 
 function App() {
   return (
--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,21 @@
-import React from 'react'; 
+import React from 'react';
+import author from "../jeff-image.jpeg"
 
 const About = () => {
 
     return (
 
-        <div id="about">
-            <header>
-                <h2>About</h2>
-                <p>Motivated and driven professional. Strong technologist who is passionate about creating great user experiences</p>
-            </header>
+        <div className="container py-5" id="about">
+            <div className="row">
+                <div className="col-lg-6 col-xm-12">
+                    <img src={author} alt="author..."/>
+                </div>
+                
+                <div className="col-lg-6 col-xm-12">
+                    <h1>About me</h1>
+                    <p>Full stack software engineer with experience in Ruby on Rails, JavaScript, and React. </p>
+                </div>
+            </div>
             
         </div>
     )
