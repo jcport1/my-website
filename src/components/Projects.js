6a02f16a51ee672031402e6f348995a05a348eb3
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-05-19 18:09:20 -0400

    Images added to Project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,7 @@
-import React from 'react'; 
+import React from 'react';
+import pawpal from "../images/pawpals.jpeg";
+import french from "../images/frenchified.jpeg"; 
+import art from "../images/art-explorer.jpeg";
 
 const Projects = () => {
 
@@ -7,9 +10,14 @@ const Projects = () => {
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">Portfolio</h1>
                     <div className="image-box-wrapper row justify-content-center">
-                        <img className="portfolio-image" src= alt=""/>
-
+                        <img className="portfolio-image" src={pawpal} alt="PawPal Ruby Project"/>
                     </div>
+                    <div className="image-box-wrapper row justify-content-center">
+                        <img className="portfolio-image" src={french} alt="French Flashcards Javascript Project"/>
+                    </div>
+                    <div className="image-box-wrapper row justify-content-center">
+                    <img className="portfolio-image" src={art} alt="Art Explorer React Project"/>
+                </div>
               
             </div>
         
