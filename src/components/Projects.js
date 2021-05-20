d2ff8489f368557bc74329808b5fbf485f84ff12
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-05-20 10:24:45 -0400

    Adds search icon to project images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,8 @@ import React from "react";
 import pawpal from "../images/pawpals.png";
 import french from "../images/frenchified.png"; 
 import art from "../images/art-explorer.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
 
 const Projects = () => {
 
@@ -11,19 +13,24 @@ const Projects = () => {
                 <h1 className="text-uppercase text-center py-5">Portfolio</h1>
 
                 <div className="image-box-wrapper row justify-content-center"> 
-
                     <div className="portfolio-image-box">
                         <img className="portfolio-image" src={pawpal} alt="PawPal Ruby Project..."/>
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
                     </div>
 
                
                     <div className="portfolio-image-box">
                         <img className="portfolio-image" src={french} alt="French Flashcards Javascript Project..."/>
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
                     </div>
             
 
                     <div className="portfolio-image-box">
                         <img className="portfolio-image" src={art} alt="Art Explorer React Project..."/>
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
                     </div>
                 </div>
             </div>
