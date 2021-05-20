4c3484da61a07138d2da7655abfd9a28e884e844
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-05-19 21:10:51 -0400

    Changed Bootstrap dependency version

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import pawpal from "../images/pawpals.jpeg";
-import french from "../images/frenchified.jpeg"; 
-import art from "../images/art-explorer.jpeg";
+import React from "react";
+import pawpal from "../images/pawpals.png";
+import french from "../images/frenchified.png"; 
+import art from "../images/art-explorer.png";
 
 const Projects = () => {
 
@@ -9,18 +9,24 @@ const Projects = () => {
         <div className="portfolio-wrapper">
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">Portfolio</h1>
-                    <div className="image-box-wrapper row justify-content-center">
-                        <img className="portfolio-image" src={pawpal} alt="PawPal Ruby Project"/>
+
+                <div className="image-box-wrapper row justify-content-center"> 
+
+                    <div className="portfolio-image-box">
+                        <img className="portfolio-image" src={pawpal} alt="PawPal Ruby Project..."/>
                     </div>
-                    <div className="image-box-wrapper row justify-content-center">
-                        <img className="portfolio-image" src={french} alt="French Flashcards Javascript Project"/>
+
+               
+                    <div className="portfolio-image-box">
+                        <img className="portfolio-image" src={french} alt="French Flashcards Javascript Project..."/>
                     </div>
-                    <div className="image-box-wrapper row justify-content-center">
-                    <img className="portfolio-image" src={art} alt="Art Explorer React Project"/>
+            
+
+                    <div className="portfolio-image-box">
+                        <img className="portfolio-image" src={art} alt="Art Explorer React Project..."/>
+                    </div>
                 </div>
-              
             </div>
-        
         </div>
     )
 }
